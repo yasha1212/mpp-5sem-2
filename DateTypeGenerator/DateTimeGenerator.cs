608c03ca51ee6473de3cf689a976fe31fb273a79
yasha1212 <doenglish1@mail.ru>
2020-10-07 14:39:23 +0300

    Add date generator

---
+++ DateTypeGenerator/DateTimeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using PluginInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateTimeGenerator
+{
+    public class DateTimeGenerator : IGenerator
+    {
+        private readonly Random random;
+
+        public DateTimeGenerator()
+        {
+            random = new Random();
+        }
+
+        public bool CanGenerate(Type type)
+        {
+            return type == typeof(DateTime);
+        }
+
+        public object Next(Type type)
+        {
+            return new DateTime(random.Next(0, DateTime.Now.Year), random.Next(1, 12), random.Next(1, 30));
+        }
+    }
+}
