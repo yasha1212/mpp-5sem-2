8711bcecfde3e154bb9f2579aae81d3f9de7adda
yasha1212 <doenglish1@mail.ru>
2020-10-07 14:01:10 +0300

    Add DTO attribute

---
+++ Faker/DTOAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faker
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class DTOAttribute : Attribute
+    {
+    }
+}
